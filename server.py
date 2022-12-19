99b8c9c24693c044cd2d7716c38ccb2008f54eed
Aleksandra Makhrova <amakhro@gmail.com>
2022-12-19 13:18:42 -0800

    "homepage.html was created"

--- server.py
+++ server.py
@@ -1,4 +1,14 @@
 from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
+@app.route("/")
+def homepage():
+    """View homepage."""
+
+    return render_template("homepage.html")
