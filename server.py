541cf1f2e95e3fd1b80b9c480b3a3cabd4e8c673
Aleksandra Makhrova <amakhro@gmail.com>
2022-12-22 21:25:30 -0800

    Login form was added

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
 from jinja2 import StrictUndefined
 import requests
+import crud
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -30,6 +31,48 @@ def homepage():
     return render_template('homepage.html', books = results)
 
 
+@app.route("/register")
+def registration_form():
+
+    return render_template("registration_form.html")
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Create a new user."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash("Cannot create an account with that email. Try again.")
+    else:
+        user = crud.create_user(email, password, request.form.get("nickname", None), 
+            request.form.get("user_picture", None), request.form.get("zipcode", None))
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created! Please log in.")
+
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Process user login."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect.")
+    else:
+        # Log in user by storing the user's email in session
+        session["user_email"] = user.email
+        flash(f"Welcome back, {user.email}!")
+
+    return redirect("/")
+
 if __name__ == '__main__':
+    connect_to_db(app)
     app.debug = True
     app.run(host='0.0.0.0')
